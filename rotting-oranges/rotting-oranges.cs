a5bbb81ab433bc94e69dd528f696fcf3d6006724
bhargavajonnalagadda <74167176+bhargavajonnalagadda@users.noreply.github.com>
2020-12-20 13:55:23 -0500

    Time: 108 ms (14.63%), Space: 26.5 MB (43.63%) - LeetHub

--- rotting-oranges/rotting-oranges.cs
+++ rotting-oranges/rotting-oranges.cs
@@ -1,3 +1,21 @@
+public class Solution {
+    int rottencount=0;
+    int freshcount =0;
+    int time =0;
+    public int OrangesRotting(int[][] grid) {
+        int rows = grid.Length;
+        int columns = grid[0].Length;
+        Queue<(int i ,int j)> q = new Queue<(int i, int j)>();
+        
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<columns;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.Enqueue((i,j));
+                }
+                else if(grid[i][j]==1)
                 {
                     freshcount++;
                 }
@@ -27,34 +45,3 @@
                 }
                 
                 if(x>0 && grid[x-1][y]==1)
-                {
-                     q.Enqueue((x-1,y));
-                    grid[x-1][y]=2;
-                    freshcount--;
-                }
-                if(y>0 && grid[x][y-1]==1)
-                {
-                     q.Enqueue((x,y-1));
-                    grid[x][y-1]=2;
-                    freshcount--;
-                }
-                Console.WriteLine(x);
-                if(x<grid.Length-1 && grid[x+1][y]==1)
-                {
-                  q.Enqueue((x+1,y));
-                    grid[x+1][y]=2;
-                    freshcount--;   
-                }
-                 
-                
-                qsize--;
-                
-            }
-            time++;
-        }
-        
-        Console.WriteLine(freshcount);
-        if(freshcount>0)
-        {
-            return -1;
-        }
