bb78a52e77d97e6004f112b71f0e97263ff20b61
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-06-24 17:21:39 +0530

    Completed Problem 2

--- Problem1.py
+++ Problem1.py
@@ -2,3 +2,38 @@
 # Space Complexity : 
 # Did this code successfully run on Leetcode : 
 # Any problem you faced while coding this : 
+
+#bfs and make rotten when adding into queue so we don't add same neighbour again and again
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        fresh = 0
+        queue = deque()
+        time = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    queue.append([i,j])
+                if grid[i][j] == 1:
+                    fresh += 1
+        if fresh == 0:
+            return 0
+        while len(queue) != 0:
+            size = len(queue)
+            dirs = [[-1,0],[1,0],[0,1],[0,-1]]
+            for i in range(size):
+                node = queue.popleft()
+                for j in dirs:
+                    x = node[0] + j[0]
+                    y = node[1] + j[1]
+                    if x >= 0 and x < m and y >= 0 and y < n and grid[x][y] == 1:
+                        grid[x][y] = 2
+                        queue.append([x,y])
+                        fresh -= 1
+            time += 1
+        if fresh != 0:
+            return -1
+        return time - 1
+        
