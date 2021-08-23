7a58ac2a38a00894423b652538eb72db97fd6ffb
Durgesh Patil <patildurgesh1998@gmail.com>
2021-08-22 20:06:40 -0400

    Create Problem-2.cpp

---
+++ Problem-2.cpp
@@ -0,0 +1,76 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+Time Complexity = O(N)
+Space Complexity = O(N)
+where N is the number of nodes on the tree and h is the height of the tree.
+*/
+class Solution {
+public:
+    int sum=0;
+    map <int, Employee*> m;
+    int getImportance(vector<Employee*> employees, int id) {
+        int i;
+        for(i=0;i<employees.size();i++)
+            m[employees[i]->id] = employees[i];
+        dfs(employees,id);
+        return sum;
+    }
+    void dfs(vector<Employee*> employees, int id)
+    {
+        //basic
+        
+        //logic
+        Employee* E = m[id];
+        sum+=E->importance;
+        int l = E->subordinates.size();
+        vector<int> sub(E->subordinates);
+        if(l)
+        {
+            for(int i=0;i<l;i++)
+                dfs(employees,sub[i]);
+        }
+    }
+};
+
+
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+Time Complexity = O(N)
+Space Complexity = O(N)
+where N is the number of nodes on the tree and h is the height of the tree.
+*/
+class Solution {
+public:
+    int sum=0;
+    map <int, Employee*> m;
+    int getImportance(vector<Employee*> employees, int id) {
+        int i;
+        for(i=0;i<employees.size();i++)
+            m[employees[i]->id] = employees[i];
+        queue <int> q;
+        q.push(id);
+        while(!q.empty())
+        {
+            Employee* E = m[q.front()];
+            sum+=E->importance;
+            vector<int> temp(E->subordinates);
+            q.pop();
+            int l = temp.size();
+            for(i=0;i<l;i++)
+                q.push(temp[i]);
+        }
+        return sum;
+    }
+};
