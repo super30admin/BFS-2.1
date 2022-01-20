c9245b9178c24eea15a6c42c5c27b8262705969f
Kartik Ghangare <kartikghangare@Kartiks-MacBook-Air.local>
2022-01-20 01:49:12 -0700

    BFS-2.1  Complete

--- EmployeeImportance.cpp
+++ EmployeeImportance.cpp
@@ -1,53 +1,53 @@
 
-// //Time Complexity O(V+E) 
-// // Space Complexity O(V+E)
-// //Problem successfully executed on leetcode
-// //No problems faced while coading this.
+//Time Complexity O(V+E) 
+// Space Complexity O(V+E)
+//Problem successfully executed on leetcode
+//No problems faced while coading this.
 
 
-// #include<iostream>
-// #include<vector>
-// #include<queue>
-// #include<map>
-// using namespace std;
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+using namespace std;
 
 
 
 
 
-// // Definition for Employee.
-// class Employee {
-// public:
-//     int id;
-//     int importance;
-//     vector<int> subordinates;
-// };
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
 
 
-// class Solution {
-// public:
-//     map<int,Employee*> idToEmployee;
-//     int total = 0;
-//     int getImportance(vector<Employee*> employees, int id) {
+class Solution {
+public:
+    map<int,Employee*> idToEmployee;
+    int total = 0;
+    int getImportance(vector<Employee*> employees, int id) {
         
-//         for(auto emp : employees)
-//         {
-//             idToEmployee[emp->id]=emp;
-//         }
-//         dfs(id);
-//         return total;
-//     }
+        for(auto emp : employees)
+        {
+            idToEmployee[emp->id]=emp;
+        }
+        dfs(id);
+        return total;
+    }
     
-//     void dfs(int id)
-//     {
-//         Employee* empObj=idToEmployee[id];
-//         vector<int> subordinates=empObj->subordinates;
-//         total+=empObj->importance;
-//         for(int empId : subordinates)
-//         {
-//             dfs(empId);
-//         }
-//     }
+    void dfs(int id)
+    {
+        Employee* empObj=idToEmployee[id];
+        vector<int> subordinates=empObj->subordinates;
+        total+=empObj->importance;
+        for(int empId : subordinates)
+        {
+            dfs(empId);
+        }
+    }
     
     
-// };
+};
