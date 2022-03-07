e45e7def50733d29f93582750b3965e1cca74f20
Jana <janahari97.sjsu@gmail.com>
2022-03-07 13:26:20 -0800

    Completed BFS 2.1

---
+++ rotting oranges.py
@@ -0,0 +1,36 @@
+# // Time Complexity : O(n) where n is the size of the grid
+# // Space Complexity : O(n)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
+
+import collections
+class Solution:
+    def orangesRotting(self, grid):
+        q = collections.deque()
+        time, fresh = 0, 0
+        
+        # build initial set of rotten oranges
+        ROWS, COLS = len(grid), len(grid[0])
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2:
+                    q.append([r,c])
+                    
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        while q and fresh > 0:
+            for i in range(len(q)):
+                r,c = q.popleft()
+                for dr, dc in directions:
+                    row, col = dr + r,dc + c
+                    
+                    # if in bounds and fresh, make rotten
+                    if (row < 0 or row == len(grid) or col < 0 or col == len(grid[0]) or grid[row][col] != 1):
+                        continue
+                    grid[row][col] = 2
+                    q.append([row, col])
+                    fresh -= 1
+            time += 1
+        return time if fresh == 0 else -1
+        
