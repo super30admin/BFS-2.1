ad9e5ab54eaf4320c6df0f2ab54b3e67e48ed983
Rajendra Prasad Patil <111140820+rajendra7406-2@users.noreply.github.com>
2022-09-18 14:42:03 -0400

    added dfs method too

--- problem1.py
+++ problem1.py
@@ -1,9 +1,15 @@
 '''
 994. Rotting Oranges
 
+APPROACH 1: BFS
 TIME COMPLEXITY: O(M*N)
 SPACE COMPLEXITY: O(M*N)
-APPROACH: BFS
+LEETCODE: YES
+DIFFICULTIES: A bit. After the class.
+
+APPROACH 2: DFS
+TIME COMPLEXITY: O(M*N) ** 2
+SPACE COMPLEXITY: O(M*N)
 LEETCODE: YES
 DIFFICULTIES: A bit. After the class.
 
@@ -13,13 +19,14 @@ DIFFICULTIES: A bit. After the class.
 
 from collections import deque
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        
+    def orangesRotting(self, ggrid: List[List[int]]) -> int:
+        global dirs, grid
+        grid = ggrid
         dirs = [(-1,0), (1,0), (0,1), (0,-1)]
         
         # BFS 
-        def approach1(grid, dirs):
-            
+        def approach1():
+            global grid, dirs
             two = [] # stores original location of rotten oranges
             
             m, n = len(grid), len(grid[0])
@@ -65,4 +72,58 @@ class Solution:
             
             return mmax
         
-        return approach1(grid, dirs)
+        # DFS 
+        def approach2():
+            # count no of 2s in grid
+            global m, n, grid
+            m = len(grid)
+            n = len(grid[0])
+            twos = []
+            one_found = False
+            
+            for i in range(0, m):
+                for j in range(0, n):
+                    if grid[i][j] == 2:
+                        twos.append((i,j))
+                    elif grid[i][j] == 1:
+                        one_found = True
+            
+            if not one_found: # there are no fresh apples
+                return 0
+            
+            def dfs(r, c):
+                global m, n, dirs, grid
+                # base
+                # base case is handled in the logic
+            
+                # logic
+                for dr, dc in dirs:
+                    nr = dr + r
+                    nc = dc + c
+                    
+                    if nr >= 0 and nc >= 0 and nr < m and nc < n:
+                        if grid[nr][nc] == 1:
+                            grid[nr][nc] = grid[r][c] + 1
+                            dfs(nr, nc)
+                        elif grid[nr][nc] != (2 or 0) and grid[r][c] + 1 < grid[nr][nc]:
+                            grid[nr][nc] = grid[r][c] + 1
+                            dfs(nr, nc)
+            
+            for r, c in twos:
+                dfs( r, c)
+            
+            mmax = 0
+            for i in range(0, m):
+                for j in range(0, n):
+                    if grid[i][j] == 1:
+                        return -1
+                    elif grid[i][j] == (2 or 0):
+                        continue
+                    else:
+                        mmax = max(grid[i][j], mmax) 
+            
+            return mmax - 2         
+        
+        return approach2()
+        # return approach1()
+    
